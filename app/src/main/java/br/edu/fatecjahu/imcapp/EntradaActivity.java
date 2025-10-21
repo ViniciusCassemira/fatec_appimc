ade96480c1532144bc8df98c25b2372996669701
AlissaGabriel <alissagabriel14@gmail.com>
2025-10-21 17:09:11 -0300

    Refactor(EntradaActivity, ResultadoActivity): remove e ajusta comentários | feat(EntradaActivity): adiciona botão Confirmar Saída e ícone

--- app/src/main/java/br/edu/fatecjahu/imcapp/EntradaActivity.java
+++ app/src/main/java/br/edu/fatecjahu/imcapp/EntradaActivity.java
@@ -1,8 +1,12 @@
 package br.edu.fatecjahu.imcapp;
 
+
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,6 +14,7 @@ import android.widget.Toast;
 import android.util.Log; // mostrar log
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -17,15 +22,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class EntradaActivity extends AppCompatActivity {
 
-    /* 1. Criação de chaves públicas para armazenar os dados de altura e peso.
-       Essas chaves públicas serão usadas no objeto Intent (putExtra)
-       para enviar as informações da tela de entrada para a tela de resultados. */
-    public static final String EXTRA_PESO = "br.edu.fatecjahu.imcapp.PESO";
-    public static final String EXTRA_ALTURA = "br.edu.fatecjahu.imcapp.ALTURA";
-
     protected static final String TAG = "Marca"; //tag para log
 
-    /* 2. Declaração dos objetos Java (elementos de interface). */
+    //1. Declaração dos objetos Java (elementos de interface).
     TextView edtPeso, edtAltura;
     Button btnCalcular;
 
@@ -41,59 +40,129 @@ public class EntradaActivity extends AppCompatActivity {
             return insets;
         });
 
-        /* 3. Associar os elementos de tela (VIEW) aos objetos Java. */
+        //2. Associar os elementos de tela (VIEW) aos objetos Java.
         edtPeso = findViewById(R.id.edtPeso);
         edtAltura = findViewById(R.id.edtAltura);
         btnCalcular = findViewById(R.id.btnCalcular);
 
-        /* 4. Criar o metodo do botão Calcular. */
+        //3. Criar o metodo do botão Calcular.
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                // 5. Captura o conteúdo dos campos de texto
+                //4. Captura o conteúdo dos campos de texto
                 String pesoStr = edtPeso.getText().toString();
                 String alturaStr = edtAltura.getText().toString();
 
-                // Verifica se os campos estão preenchidos
+                //5. Verifica se os campos estão preenchidos
                 if (pesoStr.isEmpty() || alturaStr.isEmpty()) {
-                    Toast.makeText(getContext(), "Preencha peso e altura!", Toast.LENGTH_SHORT).show();
+                    alert("Preencha peso e altura!");
                     return;
                 }
 
                 Log.i(TAG, "Peso:" + pesoStr + " | Altura:" + alturaStr); //enviar log
 
                 try {
-                    // 6. Converte os valores em float
+                    //6. Converte os valores em float
                     float peso = Float.parseFloat(pesoStr.replace(",", "."));
                     float altura = Float.parseFloat(alturaStr.replace(",", "."));
 
-                    // 7. Cria o canal de comunicação Intent (da tela EntradaActivity para ResultadoActivity)
-                    Intent intent = new Intent(getContext(), ResultadoActivity.class);
+                    //7. Cria o canal de comunicação Intent (da tela EntradaActivity para ResultadoActivity)
+                    Intent intent = new Intent(getContext(0), ResultadoActivity.class);
 
-                    // ?. Criar Bundle com os dados(altura, peso, resultado)
+                    //8. Criar Bundle com os dados(altura, peso, resultado)
                     Bundle dadosIMC = new Bundle();
 
-                    // 8. Carrega os dados (peso e altura) no objeto Intent com as chaves públicas
-                    // intent.putExtra(EXTRA_PESO, peso);
-                    // intent.putExtra(EXTRA_ALTURA, altura);
-
-                    dadosIMC.putFloat("peso", peso);    //    nome = "Alex Batista";
+                    dadosIMC.putFloat("peso", peso);
                     dadosIMC.putFloat("altura", altura);
 
-                    // 9. Executa a próxima tela levando os dados
+                    //9. Executa a próxima tela levando os dados
                     intent.putExtras(dadosIMC);
                     startActivity(intent);
 
                 } catch (NumberFormatException e) {
-                    Toast.makeText(getContext(), "Insira valores numéricos válidos!", Toast.LENGTH_SHORT).show();
+                    alert("Insira valores numéricos válidos!");
                 }
             }
         });
     }
 
-    /* Metodo auxiliar para retornar o contexto da Activity */
-    private Context getContext() {
+    //10. Metodo alert para mostrar mensagens de notificação do aplicativo.
+    private void alert(String msg) {
+        //11. A classe Toast mostra uma alerta temporário muito comum no Android.
+        Toast toast = Toast.makeText(getContext(0), msg, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.BOTTOM, 0,0);
+        toast.show();
+
+        // Toast.LENGTH_LONG tem a duração de 3 a 5 segundos da mensagem de TOAST.
+        // Toast.LENGTH_SHORT tem a duração de 1 a 3 segundos da mensagem de TOAST.
+        Log.i(TAG, getClassName() + " => Mensagem mostrada ao usuário.");
+    }
+
+    private Context getContext(int value) {
+        if (value == 1) {
+            Log.i(TAG, getClassName() + " => Passou pelo Contexto atual: " + getClassName());
+        }
         return this;
+    }
+
+    //12. Cria o metodo que retorna o nome da classe atual
+    protected String getClassName() {
+        String s = getClass().getName();
+
+        return s.substring(s.lastIndexOf("."));
+    }
+
+    //13. Metodo onBackPressedDispatcher() para sair do aplicativo.
+    @SuppressLint({"MissingSuperCall", "GestureBackNavigation"})
+    @Override
+    public void onBackPressed() {
+        // Instaciação de um objeto alertDialogBuilder (Caixa de Diálogo Personalizada)
+        // da classe do tipo AlertDialog.Builder.
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        Log.i(TAG, getClassName() + " => Objeto Criado: alertDialogBuilder.");
+
+        //14. Configuração de uma Caixa de Diálogo Personalizada.
+        alertDialogBuilder.setTitle("Confirmar Saída");
+        alertDialogBuilder.setIcon(R.drawable.ic_exit); // R.drawable.ic_exit;
+        alertDialogBuilder.setMessage("Você tem certeza que deseja sair ?");
+        alertDialogBuilder.setCancelable(false);
+
+        Log.i(TAG, getClassName() + " => Objeto Configurado: alertDialogBuilder.");
+
+        //15. Configuração do botão "Sim" da Caixa de Diálogo Personalizada...
+        // ...e acionamento do metodo onClick permitindo a saída do Aplicativo,
+        // através do encerramento da Activity.
+
+        // Metodo do botão "Sim"
+        //  setPositiveButton() => DialogInterface.OnClickListener() => onClick()
+        alertDialogBuilder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish(); // Encerra a Activity.
+                // Metodo finish faz chamada de forma programática ao
+                // metodo OnDestroy() do ciclo de vida da Activity.
+                Log.i(TAG, getClassName() + ".onBackPressed() chamado. Botão Sim pressionado.");
+            }
+        });
+
+        //16. Configuração do botão "Não" da Caixa de Diálogo Personalizada...
+        // ...e mostra mensagem de cancelamento de saída do Aplicativo.
+
+        // Metodo do botão "Não"
+        //  setNegativeButton() => DialogInterface.OnClickListener() => onClick()
+        alertDialogBuilder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                alert("Você clicou em não!!!");
+                Log.i(TAG, getClassName() + ".onBackPressed() chamado. Botão Não pressionado.");
+            }
+        });
+
+        //17. Construção de uma Caixa de Diálogo Personalizada
+        // e mostra a Caixa de Diálogo criada para o usuário.
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 }
--- app/src/main/java/br/edu/fatecjahu/imcapp/ResultadoActivity.java
+++ app/src/main/java/br/edu/fatecjahu/imcapp/ResultadoActivity.java
@@ -12,11 +12,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ResultadoActivity extends AppCompatActivity {
-
-    public static final String EXTRA_PESO = "br.edu.fatecjahu.mensagemapp.PESO";
-    public static final String EXTRA_ALTURA = "br.edu.fatecjahu.mensagemapp.ALTURA";
-    public static final String EXTRA_RECOMENDACAO = "br.edu.fatecjahu.mensagemapp.RECOMENDACAO";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,26 +24,21 @@ public class ResultadoActivity extends AppCompatActivity {
         });
 
         Bundle args = getIntent().getExtras();
-        assert args != null;
-        Float     peso = args.getFloat("peso");
-        Float     altura = args.getFloat("altura");
 
-//        Intent intent = getIntent();
-//        String peso = intent.getFloatExtra(peso);
-//        String altura = intent.getFloatExtra(altura);
+        Float peso = args.getFloat("peso");
+        Float altura = args.getFloat("altura");
 
-        // Vincula os TextViews do layout
+        //1. Vincula os TextViews do layout
         TextView tvPesoResultado = findViewById(R.id.tvPesoResultado);
         TextView tvAlturaResultado = findViewById(R.id.tvAlturaResultado);
         TextView tvRecomendacao2 = findViewById(R.id.tvRecomendacao2);
 
-        // Define os textos recebidos
+        //2. Define os textos recebidos
         tvPesoResultado.setText(String.valueOf(peso));
         tvAlturaResultado.setText(String.valueOf(altura));
-        //tvRecomendacao2.setText(recomendacao);
 
-        // Configura botão Voltar
+        //3. Configura botão Voltar
         Button btnVoltar = findViewById(R.id.btnVoltar);
-        btnVoltar.setOnClickListener(v -> finish()); // Fecha a activity e volta para a anterior
+        btnVoltar.setOnClickListener(v -> finish());
     }
 }
